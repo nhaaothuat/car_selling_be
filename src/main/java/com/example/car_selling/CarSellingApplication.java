39b3f4021bbb491c80116b27b5aff238dc6dfd07
M2 C1 <hienphannguyenxuan@gmail.com>
2025-07-05 20:48:08 +0700

    first commit

---
+++ src/main/java/com/example/car_selling/CarSellingApplication.java
@@ -0,0 +1,13 @@
+package com.example.car_selling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSellingApplication.class, args);
+	}
+
+}
